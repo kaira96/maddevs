6010001c37da5d187d6f13d58f9f7d8e3223fd9f
Kairat Zhanadylov <kairatzhanadylov@gmail.com>
2025-01-25 03:03:47 +0600

    Init project

---
+++ backend/user/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# class Patient(TypedDict):
+#     id: int
+#     # дата рождения
+#     date_of_birth: date
+#     # массив строк с названиями диагнозов
+#     diagnoses: list[str]
+#     # дата создания записи
+#     created_at: datetime
